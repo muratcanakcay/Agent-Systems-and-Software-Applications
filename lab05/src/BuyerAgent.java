0d18af6802932f53d037726bf4181fb9d3b11abe
Muratcan Akcay <muratcanakcay@gmail.com>
2022-04-14 17:51:24 +0200

    add lab5 task

---
+++ lab05/src/BuyerAgent.java
@@ -0,0 +1,43 @@
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+
+public class BuyerAgent extends Agent
+{
+    @Override
+    protected void setup() {
+        addBehaviour(new TickerBehaviour(this, 60000) {
+            protected void onTick() {
+                // Update the list of agents
+                DFAgentDescription template = new DFAgentDescription();
+                ServiceDescription sd = new ServiceDescription();
+                sd.setType("selling");
+                template.addServices(sd);
+                try {
+                    DFAgentDescription[] result = DFService.search(myAgent, template);
+                    AID[] tempAgents = new AID[result.length];
+                    for (int i = 0; i < result.length; ++i) {
+                        tempAgents[i] = result[i].getName();
+                    }
+                }
+                catch (FIPAException fe) {
+                    fe.printStackTrace();
+                }
+                // Perform the request
+                // myAgent.addBehaviour(new RequestPerformer());
+            }
+        } );
+    };
+
+    @Override
+    protected void takeDown()
+    {
+        System.out.println("Goodbye, world!" + " I was " + getAID().getLocalName());
+        super.takeDown();
+    }
+}
+
---
+++ lab05/src/SellerAgent.java
@@ -0,0 +1,40 @@
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+
+public class SellerAgent extends Agent
+{
+    @Override
+    protected void setup() {
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("selling");
+        sd.setName("book-selling");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+    };
+
+    @Override
+    protected void takeDown()
+    {
+        // Deregister from the yellow pages
+        try {
+            DFService.deregister(this);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        // Close the GUI
+        // myGui.dispose();
+        // Printout a dismissal message
+        System.out.println("Seller-agent " + getAID().getName() + " terminating.");
+    }
+}
